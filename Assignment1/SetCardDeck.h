3ce3ce9b6ef732a9806a6693aa2167d9bef12c0a
Amr Lotfy <amr.lotfy@badrit.com>
2015-05-27 11:48:25 +0300

    created view for set game

---
+++ Assignment1/SetCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  SetDeck.h
+//  Assignment1
+//
+//  Created by Amr Lotfy on 5/26/15.
+//  Copyright (c) 2015 Amr Lotfy. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface SetCardDeck : Deck
+
+@end
---
+++ Assignment1/SetCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  SetDeck.m
+//  Assignment1
+//
+//  Created by Amr Lotfy on 5/26/15.
+//  Copyright (c) 2015 Amr Lotfy. All rights reserved.
+//
+
+#import "SetCardDeck.h"
+
+@implementation SetCardDeck
+
+@end
